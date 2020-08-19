d2b31778f63575e5a70f9a159f43dbe9723afd2d
Justine Hoffa <justinefarrer@yahoo.com>
2020-08-19 08:45:27 -0600

    Add project files.

---
+++ Operators.vb
@@ -0,0 +1,31 @@
+﻿'Justine Hoffa
+'RCET0265   
+'Fall 2020
+'Operators
+'repo
+
+Module Operators
+
+    Sub Main()
+        'Let's do math!!
+        'This is a comment 
+        Console.WriteLine(2 + 2)
+        Console.ReadLine()
+        'Subtraction
+        Console.WriteLine(7 - 1)
+        Console.ReadLine()
+        'Multiplication
+        Console.WriteLine(5 * 5)
+        Console.ReadLine()
+        'Normal Division
+        Console.WriteLine(7 / 3)
+        Console.ReadLine()
+        'Integer Division
+        Console.WriteLine(7 \ 3)
+        Console.ReadLine()
+        'Mod/ Remainder
+        Console.WriteLine(7 Mod 3)
+        Console.ReadLine()
+    End Sub
+
+End Module
