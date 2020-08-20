92c98152e424a7bb37f207967fe8ffcb203ba324
Justine Hoffa <justinefarrer@yahoo.com>
2020-08-20 08:32:52 -0600

    comparison

--- Operators.vb
+++ Operators.vb
@@ -7,35 +7,49 @@
 Module Operators
 
     Sub Main()
-        'Let's do math!!
-        'This is a comment 
-        Console.WriteLine(2 + 2)
+        ''Let's do math!!
+        ''This is a comment 
+        'Console.WriteLine(2 + 2)
+        'Console.ReadLine()
+        ''Subtraction
+        'Console.WriteLine(7 - 1)
+        'Console.ReadLine()
+        ''Multiplication
+        'Console.WriteLine(5 * 5)
+        'Console.ReadLine()
+        ''Normal Division
+        'Console.WriteLine(7 / 3)
+        'Console.ReadLine()
+        ''Integer Division
+        'Console.WriteLine(7 \ 3)
+        'Console.ReadLine()
+        ''Mod/ Remainder
+        'Console.WriteLine(7 Mod 3)
+        'Console.ReadLine()
+
+        ''concatenate stuff
+        'Console.WriteLine("2" + "2")
+        'Console.ReadLine()
+        ''happy times
+        'Console.WriteLine("2" & "2")
+        'Console.ReadLine()
+        ''example
+        'Console.WriteLine("Hello" & "," & " " & "World" & "!")
+        'Console.ReadLine()
+
+        'comparison
+        Console.WriteLine(1 > 3)
         Console.ReadLine()
-        'Subtraction
-        Console.WriteLine(7 - 1)
+        Console.WriteLine(1 < 3)
         Console.ReadLine()
-        'Multiplication
-        Console.WriteLine(5 * 5)
+        Console.WriteLine(1 = 3)
         Console.ReadLine()
-        'Normal Division
-        Console.WriteLine(7 / 3)
-        Console.ReadLine()
-        'Integer Division
-        Console.WriteLine(7 \ 3)
-        Console.ReadLine()
-        'Mod/ Remainder
-        Console.WriteLine(7 Mod 3)
+        Console.WriteLine(1 <> 3)
         Console.ReadLine()
 
-        'concatenate stuff
-        Console.WriteLine("2" + "2")
-        Console.ReadLine()
-        'happy times
-        Console.WriteLine("2" & "2")
-        Console.ReadLine()
-        'example
-        Console.WriteLine("Hello" & "," & " " & "World" & "!")
-        Console.ReadLine()
+
+
+
     End Sub
 
 End Module
